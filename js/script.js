88d525ad48794ded098a5544285965fc9d0e4f01
Premjeet-Singh <itsprem2193@gmail.com>
2016-10-13 14:06:15 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+var action="click";
+var speed="1200";
+
+$(document).ready(function(){
+	
+	$('li.q').on(action, function(){
+		
+		$(this).next()
+			.slideToggle(speed)
+				
+				.siblings('li.a')
+					.slideUp();
+
+		
+		var img=$(this).children('img');
+
+		
+		$('img').not(img).removeClass('rotate');
+
+		
+		img.toggleClass('rotate');
+
+	});
+
+});
